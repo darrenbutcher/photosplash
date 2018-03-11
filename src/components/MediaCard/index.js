03a7626c2dc55b24714b17f93dfdecafca965f89
Darren Butcher <work@darrenbutcher.com>
2018-03-11 00:28:07 -0400

    Add proptypes imports

--- src/components/MediaCard/index.js
+++ src/components/MediaCard/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { forbidExtraProps } from 'airbnb-prop-types'
 import styled from 'styled-components'
 
 const MediaCard = (props) => {
