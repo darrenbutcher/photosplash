d9ebc4d81690a300b132a15a5560bfdf5f3520ad
Darren Butcher <work@darrenbutcher.com>
2018-03-11 19:18:08 -0400

    Complete MediaCard Stories

--- src/stories/MediaCard.stories.js
+++ src/stories/MediaCard.stories.js
@@ -2,8 +2,51 @@ import React from 'react'
 import { storiesOf, addDecorator } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { text, object } from '@storybook/addon-knobs/react'
+import MediaCard from '../components/MediaCard'
+
+const requiredProps = {
+  type: 'image',
+  src: 'https://via.placeholder.com/300x250'
+}
 
 storiesOf('MediaCard', module)
   .add('default', () =>
-    <div>Media Card</div>
+    <MediaCard
+      src=""
+    />
+  )
+  .add('with title', () =>
+    <MediaCard
+      src={requiredProps.src}
+      body={{
+        title: text('title', 'Title')
+      }}
+    />
+  )
+  .add('with subtitle', () =>
+    <MediaCard
+      src={requiredProps.src}
+      body={{
+        subtitle: text('subtitle', 'Subtitle')
+      }}
+    />
+  )
+  .add('with description', () =>
+    <MediaCard
+      src={requiredProps.src}
+      body={{
+        description: text('description', 'Description')
+      }}
+    />
+  )
+  .add('with button', () =>
+    <MediaCard
+      src={requiredProps.src}
+      body={{
+        button: {
+          ...object('body.button', { text: 'Button' }),
+          onClick: action('on-click')
+        }
+      }}
+    />
   )
